a8469f6606d4fc4622037a9417950a398c64260e
Ali-Bhn <bahadoran.dev@gmail.com>
2026-06-27 19:51:30 +0200

    Update main.js

--- aufgabe-7-Wheather App/main.js
+++ aufgabe-7-Wheather App/main.js
@@ -1,24 +1,24 @@
-const wetter = {
-    city: "Berlin",
-    temperature: 24,
-    humidity: 61,
-    wind: 15,
-    description: "Leicht bewölkt"
-};
+let wetterDaten = document.querySelector("#weatherContainer");
 
-let weather = document.querySelector("#weatherContainer");
-let p1 = document.createElement("p");
-let p2 = document.createElement("p");
-let p3 = document.createElement("p");
-let p4 = document.createElement("p");
-let p5 = document.createElement("p");
-p1.textContent =`City: ${wetter.city}`;
-p2.textContent=`Temperatur: ${wetter.temperature} °C`;
-p3.textContent=`Feuchtigkeit: ${wetter.humidity} %`;
-p4.textContent=`Wind: ${wetter.wind} km/h`;
-p5.textContent=`beschreibung: ${wetter.description}`;
-weather.appendChild(p1);
-weather.appendChild(p2);
-weather.appendChild(p3);
-weather.appendChild(p4);
-weather.appendChild(p5);
+const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,wind_speed_10m";
+async function ladeWetter() {
+   const response = await fetch(url);
+   const data = await response.json();
+   const current = data.current;
+   renderWetter(current);
+
+}
+function renderWetter(current){
+    const p_1 = document.createElement("p");
+    const p_2 = document.createElement("p");
+    const p_3 = document.createElement("p");
+    p_1.textContent = `Temperatur: ${current.temperature_2m} °C`;
+    p_2.textContent = `Luftfeuchtigkeit: ${current.relative_humidity_2m} %`;
+    p_3.textContent = `Wind: ${current.wind_speed_10m} km/h`;
+    wetterDaten.appendChild(p_1);
+    wetterDaten.appendChild(p_2);
+    wetterDaten.appendChild(p_3);
+
+}
+ladeWetter();
+
