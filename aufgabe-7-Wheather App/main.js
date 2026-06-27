bd47418ecafc9141777221ae04d32dd38389fd69
Ali-Bhn <bahadoran.dev@gmail.com>
2026-06-27 20:02:34 +0200

    Update main.js

--- aufgabe-7-Wheather App/main.js
+++ aufgabe-7-Wheather App/main.js
@@ -1,6 +1,6 @@
-let wetterDaten = document.querySelector("#weatherContainer");
+const wetterDaten = document.querySelector("#weatherContainer");
 
-const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,wind_speed_10m";
+const url = "...latitude=52.52...";
 async function ladeWetter() {
    const response = await fetch(url);
    const data = await response.json();
@@ -9,15 +9,16 @@ async function ladeWetter() {
 
 }
 function renderWetter(current){
-    const p_1 = document.createElement("p");
-    const p_2 = document.createElement("p");
-    const p_3 = document.createElement("p");
-    p_1.textContent = `Temperatur: ${current.temperature_2m} °C`;
-    p_2.textContent = `Luftfeuchtigkeit: ${current.relative_humidity_2m} %`;
-    p_3.textContent = `Wind: ${current.wind_speed_10m} km/h`;
-    wetterDaten.appendChild(p_1);
-    wetterDaten.appendChild(p_2);
-    wetterDaten.appendChild(p_3);
+    wetterDaten.innerHTML = "";
+    const temperatureElement = document.createElement("p");
+    const humidityElement = document.createElement("p");
+    const windElement = document.createElement("p");
+    temperatureElement.textContent = `Temperatur: ${current.temperature_2m} °C`;
+    humidityElement.textContent = `Luftfeuchtigkeit: ${current.relative_humidity_2m} %`;
+    windElement.textContent = `Wind: ${current.wind_speed_10m} km/h`;
+    wetterDaten.appendChild(temperatureElement);
+    wetterDaten.appendChild(humidityElement);
+    wetterDaten.appendChild(windElement);
 
 }
 ladeWetter();
