0fb7b6e5bf673e819be74f22e4fe9a31f49484dc
Ali-Bhn <bahadoran.dev@gmail.com>
2026-06-29 12:33:54 +0200

    Update main.js

--- aufgabe-7-Wheather App/main.js
+++ aufgabe-7-Wheather App/main.js
@@ -1,12 +1,25 @@
 const wetterDaten = document.querySelector("#weatherContainer");
-
+const cityInput = document.querySelector("#cityInput");
+const searchButton = document.querySelector("#searchButton");
+searchButton.addEventListener("click", ()=>{
+    const cityName = cityInput.value.trim();
+    ladeWetter(cityName);
+})
 // const url = "...latitude=52.52...";
 async function ladeWetter(cityName) {
+    if(cityName === ""){
+        renderError("Bitte gib eine Stad ein!!!");
+        return;
+    };
     const geocodingUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=10&language=de&format=json`;
     const responseLocation = await fetch(geocodingUrl);
     const dataLocation = await responseLocation.json();
+    console.log(dataLocation);
+    if ( !dataLocation.results||dataLocation.results.length === 0 ){
+            renderError("Stadt nicht gefunden!!!") ;
+            return;
+    };
     const location = dataLocation.results[0];
-
     
     const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m`;
     const responseWetter = await fetch(weatherUrl);
@@ -33,5 +46,11 @@ function renderWetter(current){
     wetterDaten.appendChild(windElement);
 
 }
-ladeWetter("berlin");
 
+function renderError (message){
+    wetterDaten.innerHTML = "";
+    const messageText = document.createElement("p");
+    messageText.textContent = message;
+    wetterDaten.appendChild(messageText);
+
+}
